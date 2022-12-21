3e379d4c1900f47e20ec9a49e9652b4989655f55
Miri <mirifurman@gmail.com>
2022-12-21 16:40:53 +0200

    css

--- client/src/components/Symptoms.js
+++ client/src/components/Symptoms.js
@@ -1,19 +1,21 @@
 
 export function Symptoms({ arr }) {
 
-    function symptomClickHandler(e){
-console.log(e.target.innerHTML);
+    function symptomClickHandler(e) {
+        console.log(e.target.innerHTML);
     }
-    
+
     return (
-        <div className="symptoms">
-            {Array.isArray(arr)&&<h3>Please choose a symptom</h3>}
-            {Array.isArray(arr) ?
-                arr.map(element => {
-                    return (
-                    <h5 onClick={symptomClickHandler}>{element}</h5>)
-                })
-                : null}
+        <div>
+            {Array.isArray(arr) && <h3>Please choose a symptom</h3>}
+            <div className="symptoms">
+                {Array.isArray(arr) ?
+                    arr.map(element => {
+                        return (
+                            <h5 onClick={symptomClickHandler}>{element}</h5>)
+                    })
+                    : null}
+            </div>
         </div>
     )
 }
