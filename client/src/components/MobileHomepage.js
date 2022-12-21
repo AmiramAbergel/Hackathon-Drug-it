9e605b4547610dcdc7ce3a6029ce92cc000412a8
unknown <Amiram.ab@gmail.com>
2022-12-21 22:32:45 +0200

    Home page added

---
+++ client/src/components/MobileHomepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../style/mobile-homepage.css';
+
+function MobileHomepage() {
+    return (
+        <div className='mobile-homepage'>
+            <h1 className='fade-in'>Welcome to mobile Drug-it homepage!</h1>
+            <p>
+                With the help of this app, tourists can find the right drug for
+                their pain depending on the type of pain they are experiencing.
+            </p>
+            <p>Take a look around and enjoy the project. </p>
+        </div>
+    );
+}
+
+export default MobileHomepage;
