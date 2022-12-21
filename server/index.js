8ac8d2338147791f5c3ce38b8e74db7eb4378c4a
unknown <Amiram.ab@gmail.com>
2022-12-21 22:35:01 +0200

    changes made back and front

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const BASE_URL = '/api/v1';
 
 app.use(json());
-app.use(BASE_URL, indexRoute); // localhost:4000/api/v1
+app.use(BASE_URL, indexRoute); // /api/v1
 
 //run the function so we are connected to the database
 connectToDB();
