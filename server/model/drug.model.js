657268c092fa641cdb38df71ec594e227ba0616f
unknown <Amiram.ab@gmail.com>
2022-12-21 15:31:09 +0200

    fake data add + connection to mongo atlas

--- server/model/drug.model.js
+++ server/model/drug.model.js
@@ -1,31 +1,34 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 const drugSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  activeIngredient: {
-    type: String,
-    required: true
-  },
-  indication: {
-    type: String,
-    required: true
-  },
-
-  pregnancyCategory: {
-    type: String,
-    required: true
-  },
-  manufacturer: {
-    type: String,
-    required: true
-  },
-  approvedBy: {
-    type: String,
-    required: true
-  }
+    name: {
+        type: String,
+        required: true,
+    },
+    activeIngredient: {
+        type: String,
+        required: true,
+    },
+    indication: {
+        type: String,
+        required: true,
+    },
+    dosage: {
+        type: String,
+        required: true,
+    },
+    pregnancyCategory: {
+        type: String,
+        required: true,
+    },
+    manufacturer: {
+        type: String,
+        required: true,
+    },
+    approvedBy: {
+        type: String,
+        required: true,
+    },
 });
 
 const Drug = mongoose.model('Drug', drugSchema);
