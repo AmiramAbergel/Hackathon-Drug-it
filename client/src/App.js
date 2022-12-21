317af7e13b0da317f9dc9358675f11310a705b65
unknown <Amiram.ab@gmail.com>
2022-12-21 22:20:22 +0200

    api and fetch data func added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
-
 import { Main } from './components/Main';
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 import React from 'react';
-import {Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
 import { DrugPage } from './components/DrugPage';
-import './style/DrugPage.css'
+import './style/DrugPage.css';
 import './App.css';
-import './style/main.css'
+import './style/main.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/About"/>
-      <Route path="/contact"/>
-      <Route path="/product" element={<DrugPage/>}/>
-      </Routes>
-      
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Main />} />
+                <Route path='/About' />
+                <Route path='/contact' />
+                <Route path='/product' element={<DrugPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/DrugList.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import api from '../api/api';
+
+const DrugList = ({ setIsLoading, setDrugsData, drugsData }) => {
+    const [error, setError] = useState('');
+    console.log(error);
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                setIsLoading(true);
+                const response = await api.get('/');
+                const dataArray = response.data;
+                setDrugsData(dataArray);
+            } catch (err) {
+                if (err.response) {
+                    // Not in the 200 response range
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else {
+                    console.log(err.message);
+                    setError(err.message);
+                }
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        getData();
+    }, [setIsLoading, setDrugsData]);
+    return (
+        <div>
+            <h1>Drug List</h1>
+            <ul>
+                {drugsData.map((drug) => (
+                    <li key={drug.id}>{drug.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default DrugList;
