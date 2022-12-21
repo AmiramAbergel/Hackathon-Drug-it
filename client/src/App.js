aad060ca953265c321b63100b6d7da081716b665
ahmad21-meet <ahmad2004muhtaseb@gmail.com>
2022-12-21 22:40:31 +0200

    new update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
+import ProductList from './components/product/ProductList';
+
 
 function App() {
   return (
     <div>
       <Navbar />
+      <ProductList />
+
       <div className="container">
         <article>
           <h1>What is Lorem Ipsum? </h1>
